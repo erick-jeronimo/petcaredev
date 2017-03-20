04f8b5224cd627bb2045e568cb5a21a44aaf1572
Erick Jeronimo <Erick Jeronimo>
2017-03-20 14:03:47 -0300

    teste deploy heroku

--- config/app.php
+++ config/app.php
@@ -38,7 +38,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -103,7 +103,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'jt3dy8i7Ndp7PmcNcO8Byp7Vw8pLdeVCWCXFU1Dws6Q='),
 
     'cipher' => 'AES-256-CBC',
 
