a06d3b7ee216253009a07f16fa09fb1521439164
Erick Jeronimo <Erick Jeronimo>
2017-03-20 14:05:18 -0300

    teste cleardb heroku

--- config/database.php
+++ config/database.php
@@ -38,14 +38,14 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
-
+        
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_b85c6fb92c91952'),
+            'username' => env('DB_USERNAME', 'b4dd02039ab241'),
+            'password' => env('DB_PASSWORD', '1c696087'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
